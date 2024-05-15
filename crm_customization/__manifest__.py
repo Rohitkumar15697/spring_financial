834ecde5f2347d7cc23591cd0d5c4f172b640a62
rohit kumar odoo <rohitkumar.odoo828@webkul.in>
2024-05-15 11:48:38 +0530

    [UPD] CRM opportunity creation

--- crm_customization/__manifest__.py
+++ crm_customization/__manifest__.py
@@ -4,8 +4,11 @@
         Customization for CRM module. Creating two different types
         of opportunities from one lead
     """,
-    'version': '1.0.0',
     'depends':['crm'],
+    'data':[
+        'views/crm_lead_view.xml',
+            ],
+    'version': '1.0.0',
     'application': True,
     'sequence': 1,
 }
---
+++ crm_customization/models/lead.py
@@ -0,0 +1,48 @@
+from odoo import models, fields
+from odoo.exceptions import UserError
+
+class Lead(models.Model):
+    _inherit = "crm.lead"
+
+    opportunity_type = fields.Selection([
+        ('technical', 'Technical'),
+        ('business', 'Business'),
+    ], string="Opportunity Type")
+    
+    lead_stages = fields.Selection([
+        ('new', 'NEW'),
+        ('under_review', 'Under Review'),
+        ('complete', 'Complete'),
+    ], default="new") # We can also create Many2one field for the states
+    
+
+    def convert_opportunity(self, partner, user_ids=False, team_id=False):
+        """Override complete method to get the required customization flow
+        """
+        customer = partner if partner else self.env['res.partner']
+        for lead in self:
+            if lead.lead_stages =='complete':
+                if not lead.active or lead.probability == 100:
+                    continue
+                vals = lead._convert_opportunity_data(customer, team_id)
+                lead.write(vals)
+                
+                # Create extra opportunity of opportunity type 'technical'
+                tech_lead = lead.sudo().copy()
+                vals = tech_lead.with_context(technical_opportunity=True)._convert_opportunity_data(customer, team_id)
+                tech_lead.write(vals)
+            else:
+                raise UserError('Leads can not be converted into Opportunity if it is not in Complete State!')
+            # =====================================================
+            
+        if user_ids or team_id:
+            self._handle_salesmen_assignment(user_ids=user_ids, team_id=team_id)
+        return True
+
+    def _convert_opportunity_data(self, customer, team_id=False):
+        upd_values = super()._convert_opportunity_data(customer, team_id)
+        if self._context.get('technical_opportunity'):
+            upd_values['opportunity_type'] = 'technical'
+        else:
+            upd_values['opportunity_type'] = 'business'
+        return upd_values
